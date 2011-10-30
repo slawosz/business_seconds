c4de2c8eecb252d2ccf6026ef86e9d3626628c5f
Gun.io Whitespace Robot <contact@gun.io>
2011-10-29 21:30:34 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- test/test_time_extensions.rb
+++ test/test_time_extensions.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 class TestTimeExtensions < Test::Unit::TestCase
-  
+
 
   should "know if instance is during business hours" do
     assert(!Time.during_business_hours?(Time.parse("April 11, 2010 10:45 am")))
@@ -26,7 +26,7 @@ class TestTimeExtensions < Test::Unit::TestCase
       end_time = Time.parse("August 17th, 2010, 18:50 pm")
       expecting = Time.business_seconds_duration_from_to(start_time, end_time)
       assert_equal expecting, 0
-      
+
       start_time = Time.parse("August 22th, 2010, 11:50 am")
       end_time = Time.parse("August 22th, 2010, 13:50 pm")
       expecting = Time.business_seconds_duration_from_to(start_time, end_time)
@@ -37,7 +37,7 @@ class TestTimeExtensions < Test::Unit::TestCase
       # weekend day
       start_time = Time.parse("August 22th, 2010, 11:50 am")
       BusinessTime::Config.reset
-      
+
       BusinessTime::Config.holidays << Date.parse("August 24, 2010")
       end_time = Time.parse("August 24th, 2010, 13:50 pm")
       expecting = Time.business_seconds_duration_from_to(start_time, end_time)
@@ -93,5 +93,5 @@ class TestTimeExtensions < Test::Unit::TestCase
     expecting = Time.business_seconds_from_the_beginning_of_day(time)
     assert_equal expecting, 0
   end
-  
+
 end
